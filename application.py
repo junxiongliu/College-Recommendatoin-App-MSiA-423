a107f8962a1b5eca470182c16f5cead1ff6b5bda
Junxiong Liu <tomliujx@gmail.com>
2018-03-11 23:14:07 -0500

    minor indentation fix

--- application.py
+++ application.py
@@ -32,11 +32,11 @@ def home_page():
         
     """
     if request.method == 'GET':
-    	logger.info('Going to main page.')
-    	return render_template('layout_homepage.html')
+        logger.info('Going to main page.')
+        return render_template('layout_homepage.html')
 
-#	elif request.method == 'POST':
-#		return render_template('layout_homepage.html') 
+#   elif request.method == 'POST':
+#       return render_template('layout_homepage.html') 
 
 @application.route("/recommendation",methods=['GET','POST'])
 def recommendation_page():
@@ -63,50 +63,50 @@ def recommendation_page():
     strict_criteria = [p_region, p_degree, p_schoolsize, p_schooltype]
     clustering_info = [SAT, p_major]
 
-	# connect AWS RDS
+    # connect AWS RDS
     statement = 'SELECT * FROM college'
     uri = application.config['SQLALCHEMY_DATABASE_URI']
     df = modeling.read_sql(statement,uri)
     df_filtered = modeling.filter(df,strict_criteria)
     result = modeling.modeling(df_filtered,clustering_info)
 
-	# BELOW are code specific for output
+    # BELOW are code specific for output
 
-	# insert an empty column just for output
+    # insert an empty column just for output
     num_rows = len(result.index)
     inserted_vals = ['']*num_rows
     result.insert(loc=7, column='Proportion of majors',value=inserted_vals)
 
-	# rename
+    # rename
     output_rename = {'INSTNM': 'Name', 'CITY': 'City', 'state': 'State', 'ADM_RATE': 'Admission rate', 'SATVRMID': 'Median SAT Verbal', 
-	'SATMTMID': 'Median SAT Math', 'num_undergrad': 'Number of undergraduates', 'prop_arts_human': 'Arts and Humanities', 'prop_business': 'Business',
-	'prop_health_med': 'Health and Medicine', 'prop_interdiscip': 'Multi-/Interdisciplinary Studies', 'prop_public_svce': 'Public and Social Services', 'prop_sci_math_tech': 'Science, Math, and Technology',
-	'prop_social_sci': 'Social Sciences', 'prop_trades_personal_svce': 'Trades and Personal Services'}
+    'SATMTMID': 'Median SAT Math', 'num_undergrad': 'Number of undergraduates', 'prop_arts_human': 'Arts and Humanities', 'prop_business': 'Business',
+    'prop_health_med': 'Health and Medicine', 'prop_interdiscip': 'Multi-/Interdisciplinary Studies', 'prop_public_svce': 'Public and Social Services', 'prop_sci_math_tech': 'Science, Math, and Technology',
+    'prop_social_sci': 'Social Sciences', 'prop_trades_personal_svce': 'Trades and Personal Services'}
     result_output = result.rename(index=str, columns=output_rename)
 
-	# fill na with '' and format numbers in [0,1] to %
+    # fill na with '' and format numbers in [0,1] to %
     names_to_format = ['Admission rate', 'Arts and Humanities', 'Business', 'Health and Medicine', 'Multi-/Interdisciplinary Studies', 'Public and Social Services', 'Science, Math, and Technology', 'Social Sciences', 'Trades and Personal Services']
     for name in names_to_format:
-    	result_output[name] = result_output[name].map('{:,.2%}'.format)
+        result_output[name] = result_output[name].map('{:,.2%}'.format)
 
-	# also fill na with ''
+    # also fill na with ''
     result_output = result_output.fillna('N/A')
-    result_output['Admission rate'] = result_output['Admission rate'].str.replace('nan%', 'N/A')	
+    result_output['Admission rate'] = result_output['Admission rate'].str.replace('nan%', 'N/A')    
 
-	# transpose to output
+    # transpose to output
     result_output = result_output.set_index('Name').T
 
     try:
-	    if request.method == 'POST':
-	    	logger.info('Prediction generated.')
-	    	return render_template('layout_predictionpage.html',data=result_output.to_html())
-	except:
-		logger.warning('Unexpected User Inputs resulting in unable to load prediction page. Refreshing main page.')
-	    return render_template('layout_homepage.html')
-	    
-	# if request.method == 'GET':
-	# 	print ("hi")
-	# return render_template('layout_predictionpage.html',**locals())
+        if request.method == 'POST':
+            logger.info('Prediction generated.')
+            return render_template('layout_predictionpage.html',data=result_output.to_html())
+    except:
+        logger.warning('Unexpected User Inputs resulting in unable to load prediction page. Refreshing main page.')
+        return render_template('layout_homepage.html')
+        
+    # if request.method == 'GET':
+    #   print ("hi")
+    # return render_template('layout_predictionpage.html',**locals())
 
 if __name__ == "__main__":
-	application.run(host = '0.0.0.0', use_reloader=True, debug=True)
+    application.run(host = '0.0.0.0', use_reloader=True, debug=True)
